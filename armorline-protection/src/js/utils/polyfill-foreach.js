048164163d6aa74d61e0d63f5cee2caada66fb41
Dima Bellavin <izume@inbox.ru>
2020-08-17 17:19:43 +0300

    inmar

--- armorline-protection/src/js/utils/polyfill-foreach.js
+++
@@ -1,7 +0,0 @@
-const forEachPolyfill = () => {
-  if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = Array.prototype.forEach;
-  }
-};
-
-export {forEachPolyfill};
--- build/js/main.readonly.js
+++ build/js/main.readonly.js
@@ -261,14 +261,14 @@ export const contacts = () => {
 
   // const mapElem = document.querySelector(`.js-contacts-map`);
   // if (mapElem) {
-    let map;
+    // let map;
 
-    function initMap() {
-      map = new google.maps.Map(mapElem, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-      });
-    }
+    // function initMap() {
+    //   map = new google.maps.Map(mapElem, {
+    //     center: { lat: -34.397, lng: 150.644 },
+    //     zoom: 8
+    //   });
+    // }
   // }
 }
 
